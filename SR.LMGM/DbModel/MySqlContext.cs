7100fd98699e3e6d5fda886084cd1afeac495f83
Sreejith <sreejith.kumar@intellizenz.in>
2020-06-01 15:42:47 +0530

    updated context and styles.

---
+++ SR.LMGM/DbModel/MySqlContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SR.LMGM.Models;
+
+namespace SR.LMGM.DbModel
+{
+    public partial class MySqlContext : DbContext
+    {
+        public MySqlContext()
+        {
+        }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL(Helper.SRConnection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Designation).HasColumnName("designation");
+
+                entity.Property(e => e.Empcode)
+                    .IsRequired()
+                    .HasColumnName("empcode")
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Empcont)
+                    .IsRequired()
+                    .HasColumnName("empcont")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Empname)
+                    .IsRequired()
+                    .HasColumnName("empname")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Managedby)
+                    .HasColumnName("managedby")
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasColumnName("username")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SR.LMGM/DbModel/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR.LMGM.DbModel
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public string Empcode { get; set; }
+        public string Empname { get; set; }
+        public string Empcont { get; set; }
+        public int Designation { get; set; }
+        public string Managedby { get; set; }
+    }
+}
